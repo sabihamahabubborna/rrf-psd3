fb8bd81fbab1f3d0bf0c8cc87a2071e719372e1b
sabihamahabubborna <30389227+sabihamahabubborna@users.noreply.github.com>
2017-08-18 23:38:53 +0600

    psd 3

---
+++ js/main.js
@@ -0,0 +1,14 @@
+jQuery( document ).ready(function($) {
+  $(".homepage-slides").owlCarousel({
+
+  	items:1,
+  	
+  	loop:true,
+  	nav:true,
+  	autoplay:true,
+  
+  	navText:["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
+  
+  autoplay:false
+}); 
+});
